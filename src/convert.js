47c027dfb460777c3d01eb03614a5736fd874b42
Michael C <michael@mchang.name>
2021-11-14 00:10:04 -0500

    initial commit

---
+++ src/convert.js
@@ -0,0 +1,29 @@
+// consts
+const apiURL = "https://sponsor.ajay.app/api/segmentInfo"
+
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err)=> new Response(err.stack, { status: 500 })
+    )
+  );
+});
+
+const getData = async (UUID) => {
+  const res = await fetch(`${apiURL}?UUID=${UUID}`)
+  return res.json()
+}
+
+const redirect = (url) => new Response(url, {
+  status: 301, headers: { 'Location': url }
+});
+
+async function handleRequest(request) {
+  const { pathname, searchParams } = new URL(request.url)
+  const UUID = pathname.substring(1)
+  const redir = searchParams.get('redir')
+  const data = await getData(UUID)
+  const { videoID, startTime } = data[0]
+  const newURL = `https://www.youtube.com/watch?v=${videoID}&t=${Math.max(0, startTime.toFixed(0)-2)}s#requiredSegment=${UUID}`
+  return (redir) ? redirect(newURL) : new Response(newURL)
+}
