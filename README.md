fd4a8bcafad9ed860822d74a47bd17e9651b9873
Michael C <michael@mchang.name>
2022-07-26 15:12:56 -0400

    add error handling, redirects
    - add custom redirects
    - regex for UUIDs
    - clean up code
    - remove partial video lookup in favor of Options
    - bump uglify-js

--- README.md
+++ README.md
@@ -3,11 +3,14 @@ WebWorker to return youtube link with videoID, requiredSegment and startTime giv
 
 ```
 https://sb.mchang.xyz/:UUID?api=false
-https://sb.mchang.xyz/:videoID/:UUID?api=false
+https://sb.mchang.xyz/:UUID/:Option?api=false
 
-videoID: youTube videoID
-UUID: 65 character UUID of the sponsorBlock segment
+UUID: 64/65 character UUID of the SponsorBlock segment
+Option:
+ - `video` - redirects to youtube video
+ - `info` - redirects to the /segementinfo api endpoints
+ - `db` - redirects to sb.ltn.fi uuid page
 api: if truthy, return json instead of redirect
 ```
 
-API is licenced uner CC BY-NC-SA 4.0 https://sponsor.ajay.app
+API is licenced under CC BY-NC-SA 4.0 https://sponsor.ajay.app
--- src/convert.js
+++ src/convert.js
@@ -1,41 +1,62 @@
 // consts
 const apiURL = "https://sponsor.ajay.app/api/segmentInfo"
 
-addEventListener("fetch", (event) => {
+addEventListener("fetch", event =>
   event.respondWith(
     handleRequest(event.request).catch(
       (err) => new Response(err.stack, { status: 500 })
     )
-  );
-});
+  )
+)
 
 const redirect = (url) => new Response(url, {
   status: 301, headers: { 'Location': url }
 });
 
+function handleInvalidUUID(uuid) {
+  const customLinks = {
+    "abuse": "https://mchangrh.github.io/sb-tools-index/sponsorblock.png",
+    "ports": "https://docs.google.com/spreadsheets/d/1vxSx06dPs_X3WCCC0bMztmlxbg_ETzAmp7sL70HFIew/edit",
+    "index": "https://github.com/mchangrh/sb-tools-index#readme"
+  }
+  const link = customLinks?.[uuid]
+  return link ? redirect(link) : new Response(`Invalid UUID: ${uuid}`, { status: 400 });
+}
+
+const fetchData = (UUID) => fetch(`${apiURL}?UUID=${UUID}`)
+  .then(res => res.json())
+  .catch(null)
+
 async function handleRequest(request) {
   const { pathname, searchParams } = new URL(request.url)
-  const pathArr = pathname.split("/").filter(x => x)
+  const pathArr = pathname.split("/")
   let newURL
   const length = pathArr.length
-
-  if (length === 0) {
-    return redirect("https://github.com/mchangrh/sb-lookup")
-  } else if (length === 1) {
+  if (length === 0) return redirect("https://github.com/mchangrh/sb-lookup")
+  else if (length === 1) {
     const UUID = pathArr[0]
-    const data = await fetch(`${apiURL}?UUID=${UUID}`)
-      .then(res => res.json())
-      .catch(() => null)
-    if (!data) {
-      return new Response(null, { status: 404 })
-    }
+    if (!UUID.match(/[a-f0-9]{64,65}/)) return handleInvalidUUID(UUID)
+    const data = await fetchData()
+    if (!data) return new Response(null, { status: 404 })
     const { videoID, startTime } = data[0]
     newURL = `https://www.youtube.com/watch?v=${videoID}&t=${Math.max(0, startTime.toFixed(0)-2)}s#requiredSegment=${UUID}`
   } else if (length === 2) {
-    const [ videoID, UUID ] = pathArr
-    newURL = `https://www.youtube.com/watch?v=${videoID}#requiredSegment=${UUID}`
+    const [UUID, option] = pathArr
+    // video | db | info
+    if (option === "db") {
+      newURL = `https://sb.ltn.fi/uuid/${UUID}`
+    } else if (option === "info") {
+      newURL = `${apiURL}?UUID=${UUID}`
+    } else if (option === "video") {
+      const data = await fetchData()
+      if (!data) return new Response(null, { status: 404 })
+      newURL = `https://youtube.com/watch?v=${data[0].videoID}`
+    } else {
+      return new Response("invalid option, please use db, info, or video", { status: 400 })
+    }
+  } else {
+    return new Response("invalid path", { status: 400 })
   }
-  // return
   const api = searchParams.get('api')
   return (api) ? new Response(newURL) : redirect(newURL)
 }
