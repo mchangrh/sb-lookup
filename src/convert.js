589be0af131e9eec3880a9083842873e105404b7
Michael C <michael@mchang.name>
2022-11-01 15:18:27 -0400

    dont add s parameter twice

--- src/convert.js
+++ src/convert.js
@@ -43,7 +43,7 @@ async function handleRequest(request) {
     if (!data) return new Response(null, { status: 404 })
     const { videoID, startTime } = data[0]
     const timeParam = startTime > 0 ? `t=${Math.floor(startTime)}s` : ""
-    newURL = `https://www.youtube.com/watch?v=${videoID}${timeParam}s#requiredSegment=${UUID}`
+    newURL = `https://www.youtube.com/watch?v=${videoID}${timeParam}#requiredSegment=${UUID}`
   } else if (length === 2) {
     const [UUID, option] = pathArr
     // video | db | info
